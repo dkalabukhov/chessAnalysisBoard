984281ef72a2015757d03ef0816af243d0c6c0ad
dkalabukhov <daniil.kalabukhov@gmail.com>
2024-04-02 21:06:03 +0300

    Setted up chessboard

---
+++ src/index.js
@@ -0,0 +1,480 @@
+const matrix = [
+  [{
+    name: 'a8',
+    contains: {
+      type: 'rook',
+      color: 'black',
+    },
+  }, {
+    name: 'b8',
+    contains: {
+      type: 'knight',
+      color: 'black',
+    },
+  }, {
+    name: 'c8',
+    contains: {
+      type: 'bishop',
+      color: 'black',
+    },
+  }, {
+    name: 'd8',
+    contains: {
+      type: 'queen',
+      color: 'black',
+    },
+  }, {
+    name: 'e8',
+    contains: {
+      type: 'king',
+      color: 'black',
+    },
+  }, {
+    name: 'f8',
+    contains: {
+      type: 'bishop',
+      color: 'black',
+    },
+  }, {
+    name: 'g8',
+    contains: {
+      type: 'knight',
+      color: 'black',
+    },
+  }, {
+    name: 'h8',
+    contains: {
+      type: 'rook',
+      color: 'black',
+    },
+  }],
+  [{
+    name: 'a7',
+    contains: {
+      type: 'pawn',
+      color: 'black',
+    },
+  }, {
+    name: 'b7',
+    contains: {
+      type: 'pawn',
+      color: 'black',
+    },
+  }, {
+    name: 'c7',
+    contains: {
+      type: 'pawn',
+      color: 'black',
+    },
+  }, {
+    name: 'd7',
+    contains: {
+      type: 'pawn',
+      color: 'black',
+    },
+  }, {
+    name: 'e7',
+    contains: {
+      type: 'pawn',
+      color: 'black',
+    },
+  }, {
+    name: 'f7',
+    contains: {
+      type: 'pawn',
+      color: 'black',
+    },
+  }, {
+    name: 'g7',
+    contains: {
+      type: 'pawn',
+      color: 'black',
+    },
+  }, {
+    name: 'h7',
+    contains: {
+      type: 'pawn',
+      color: 'black',
+    },
+  }],
+  [{
+    name: 'a6',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'b6',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'c6',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'd6',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'e6',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'f6',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'g6',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'h6',
+    contains: {
+      type: null,
+    },
+  }],
+  [{
+    name: 'a5',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'b5',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'c5',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'd5',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'e5',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'f5',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'g5',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'h5',
+    contains: {
+      type: null,
+    },
+  }],
+  [{
+    name: 'a4',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'b4',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'c4',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'd4',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'e4',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'f4',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'g4',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'h4',
+    contains: {
+      type: null,
+    },
+  }],
+  [{
+    name: 'a3',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'b3',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'c3',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'd3',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'e3',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'f3',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'g3',
+    contains: {
+      type: null,
+    },
+  }, {
+    name: 'h3',
+    contains: {
+      type: null,
+    },
+  }],
+  [{
+    name: 'a2',
+    contains: {
+      type: 'pawn',
+      color: 'white',
+    },
+  }, {
+    name: 'b2',
+    contains: {
+      type: 'pawn',
+      color: 'white',
+    },
+  }, {
+    name: 'c2',
+    contains: {
+      type: 'pawn',
+      color: 'white',
+    },
+  }, {
+    name: 'd2',
+    contains: {
+      type: 'pawn',
+      color: 'white',
+    },
+  }, {
+    name: 'e2',
+    contains: {
+      type: 'pawn',
+      color: 'white',
+    },
+  }, {
+    name: 'f2',
+    contains: {
+      type: 'pawn',
+      color: 'white',
+    },
+  }, {
+    name: 'g2',
+    contains: {
+      type: 'pawn',
+      color: 'white',
+    },
+  }, {
+    name: 'h2',
+    contains: {
+      type: 'pawn',
+      color: 'white',
+    },
+  }],
+  [{
+    name: 'a1',
+    contains: {
+      type: 'rook',
+      color: 'white',
+    },
+  }, {
+    name: 'b1',
+    contains: {
+      type: 'knight',
+      color: 'white',
+    },
+  }, {
+    name: 'c1',
+    contains: {
+      type: 'bishop',
+      color: 'white',
+    },
+  }, {
+    name: 'd1',
+    contains: {
+      type: 'queen',
+      color: 'white',
+    },
+  }, {
+    name: 'e1',
+    contains: {
+      type: 'king',
+      color: 'white',
+    },
+  }, {
+    name: 'f1',
+    contains: {
+      type: 'bishop',
+      color: 'white',
+    },
+  }, {
+    name: 'g1',
+    contains: {
+      type: 'knight',
+      color: 'white',
+    },
+  }, {
+    name: 'h1',
+    contains: {
+      type: 'rook',
+      color: 'white',
+    },
+  }],
+
+];
+
+const figures = {
+  pawn: {},
+  rook: {},
+  king: {},
+  queen: {},
+  knight: {},
+  bishop: {},
+};
+
+function createPieces() {
+  const wRook = document.createElement('img');
+  wRook.setAttribute('src', './assets/wR.svg');
+
+  const wKnight = document.createElement('img');
+  wKnight.setAttribute('src', './assets/wN.svg');
+
+  const wBishop = document.createElement('img');
+  wBishop.setAttribute('src', './assets/wB.svg');
+
+  const wQueen = document.createElement('img');
+  wQueen.setAttribute('src', './assets/wQ.svg');
+
+  const wKing = document.createElement('img');
+  wKing.setAttribute('src', './assets/wK.svg');
+
+  const wPawn = document.createElement('img');
+  wPawn.setAttribute('src', './assets/wP.svg');
+
+  const bRook = document.createElement('img');
+  bRook.setAttribute('src', './assets/bR.svg');
+
+  const bKnight = document.createElement('img');
+  bKnight.setAttribute('src', './assets/bN.svg');
+
+  const bBishop = document.createElement('img');
+  bBishop.setAttribute('src', './assets/bB.svg');
+
+  const bQueen = document.createElement('img');
+  bQueen.setAttribute('src', './assets/bQ.svg');
+
+  const bKing = document.createElement('img');
+  bKing.setAttribute('src', './assets/bK.svg');
+
+  const bPawn = document.createElement('img');
+  bPawn.setAttribute('src', './assets/bP.svg');
+
+  return {
+    bRook,
+    bKnight,
+    bBishop,
+    bQueen,
+    bKing,
+    bPawn,
+    wRook,
+    wKnight,
+    wBishop,
+    wQueen,
+    wKing,
+    wPawn,
+  };
+}
+
+const board = document.querySelector('.board');
+function render() {
+  matrix.forEach((row) => {
+    row.forEach((cell) => {
+      const pieces = createPieces();
+      const [cellY, cellX] = cell.name.split('');
+      const currRow = board.querySelector(`[data-row="${cellX}"]`);
+      const currCell = currRow.querySelector(`[data-cell="${cellY}"]`);
+      switch (cell.contains.type) {
+        case 'pawn': {
+          cell.contains.color === 'white'
+            ? currCell.replaceChildren(pieces.wPawn)
+            : currCell.replaceChildren(pieces.bPawn);
+          break;
+        }
+        case 'rook': {
+          cell.contains.color === 'white'
+            ? currCell.replaceChildren(pieces.wRook)
+            : currCell.replaceChildren(pieces.bRook);
+          break;
+        }
+        case 'knight': {
+          cell.contains.color === 'white'
+            ? currCell.replaceChildren(pieces.wKnight)
+            : currCell.replaceChildren(pieces.bKnight);
+          break;
+        }
+        case 'bishop': {
+          cell.contains.color === 'white'
+            ? currCell.replaceChildren(pieces.wBishop)
+            : currCell.replaceChildren(pieces.bBishop);
+          break;
+        }
+        case 'queen': {
+          cell.contains.color === 'white'
+            ? currCell.replaceChildren(pieces.wQueen)
+            : currCell.replaceChildren(pieces.bQueen);
+          break;
+        }
+        case 'king': {
+          cell.contains.color === 'white'
+            ? currCell.replaceChildren(pieces.wKing)
+            : currCell.replaceChildren(pieces.bKing);
+          break;
+        }
+        default: {
+          return null;
+        }
+      }
+    });
+  });
+}
+
+render();
