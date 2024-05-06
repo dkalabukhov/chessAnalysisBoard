23642f374a49cceafaf4ccac55849916f8722cfe
sseezov <96130056+sseezov@users.noreply.github.com>
2024-05-06 23:57:19 +0700

    123 (#3)

--- src/move.js
+++ src/move.js
@@ -1,3 +1,9 @@
-export default () => {
-  
+export default (matrix, targetCell, figure) => {
+  if (!targetCell) {
+    return false
+  }
+  const [tcell, trow] = targetCell.split('');
+  const [currentCell, currentRow] = figure.split('');
+  matrix[trow][tcell].contains = matrix[currentRow][currentCell].contains;
+  matrix[currentRow][currentCell].contains = { type: null }
 }
