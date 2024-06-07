995cdcdfe3c4b3e8e09cd421779963d75d1e45d2
dkalabukhov <daniil.kalabukhov@gmail.com>
2024-06-07 17:47:49 +0300

    Added moves render, uninished

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const domBoard = document.querySelector('.board');
 const turn = document.querySelector('.info__turn');
 const fenForm = document.querySelector('.fen__form');
 const fenInput = document.querySelector('.fen__input');
+const table = document.querySelector('tbody');
 
 const state = {
   cursor: 'idle',
@@ -33,6 +34,76 @@ const render = () => {
   board.cellNames.forEach((name) => {
     const domCell = document.querySelector(`[data-cell="${name}"]`);
     renderCell(board.cellByName(name), domCell);
+  });
+};
+
+const renderMovesTable = () => {
+  table.innerHTML = '';
+  const keys = Object.keys(board.turnsHistory);
+  keys.forEach((key) => {
+    const { figure, move, turn } = board.turnsHistory[key];
+    const { white, black } = move;
+    const tr = document.createElement('tr');
+    const turnNumber = document.createElement('td');
+    const whiteMoveCell = document.createElement('td');
+    const blackMoveCell = document.createElement('td');
+    const whiteIcon = document.createElement('img');
+    const blackIcon = document.createElement('img');
+    switch (figure.white) {
+      case 'pawn':
+        whiteIcon.setAttribute('src', './assets/wP.svg');
+        break;
+      case 'knight':
+        whiteIcon.setAttribute('src', './assets/wN.svg');
+        break;
+      case 'bishop':
+        whiteIcon.setAttribute('src', './assets/wB.svg');
+        break;
+      case 'rook':
+        whiteIcon.setAttribute('src', './assets/wR.svg');
+        break;
+      case 'queen':
+        whiteIcon.setAttribute('src', './assets/wQ.svg');
+        break;
+      case 'king':
+        whiteIcon.setAttribute('src', './assets/wK.svg');
+        break;
+      default:
+        break;
+    }
+    switch (figure.black) {
+      case 'pawn':
+        blackIcon.setAttribute('src', './assets/bP.svg');
+        break;
+      case 'knight':
+        blackIcon.setAttribute('src', './assets/bN.svg');
+        break;
+      case 'bishop':
+        blackIcon.setAttribute('src', './assets/bB.svg');
+        break;
+      case 'rook':
+        blackIcon.setAttribute('src', './assets/bR.svg');
+        break;
+      case 'queen':
+        blackIcon.setAttribute('src', './assets/bQ.svg');
+        break;
+      case 'king':
+        blackIcon.setAttribute('src', './assets/bK.svg');
+        break;
+      default:
+        break;
+    }
+
+    turnNumber.textContent = turn;
+    turnNumber.classList.add('table-secondary');
+    whiteMoveCell.textContent = white;
+    if (figure.white) whiteMoveCell.prepend(whiteIcon);
+    whiteMoveCell.classList.add('table-light');
+    blackMoveCell.textContent = black;
+    if (figure.black) blackMoveCell.prepend(blackIcon);
+    blackMoveCell.classList.add('table-light');
+    tr.append(turnNumber, whiteMoveCell, blackMoveCell);
+    table.append(tr);
   });
 };
 
@@ -70,6 +141,7 @@ domBoard.addEventListener('click', (e) => {
         board.clearCheck();
         board.startNewTurn();
         state.turn = board.currentTurnColor;
+        renderMovesTable();
       }
       state.cursor = 'idle';
       board.cleanEffects();
