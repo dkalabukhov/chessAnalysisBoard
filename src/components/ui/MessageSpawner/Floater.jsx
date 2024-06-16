2726559fc5b59f59d594414dbc8d300171afc48c
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2024-06-16 16:52:14 +0300

    fix floatin messages inGame

--- src/components/ui/MessageSpawner/Floater.jsx
+++ src/components/ui/MessageSpawner/Floater.jsx
@@ -11,7 +11,7 @@ const Floater = ({ from, text, onClose }) => {
   }, [onClose]);
 
   return ReactDOM.createPortal(
-    <div className="modal">
+    <div className="floaterModal">
 
       {`${from}: ${text}`}
     </div>,
