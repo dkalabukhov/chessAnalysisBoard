808fff53bc669a4b7c4a4d073090aa9b5099c22f
Allegro <alex.makovka@gmail.com>
2024-06-16 11:08:07 +0300

    playpage: added board marking

--- src/components/Pages/PlayRoomPage.jsx
+++ src/components/Pages/PlayRoomPage.jsx
@@ -126,9 +126,11 @@ const PlayRoomPage = () => {
         case 'winProposal':
           // eslint-disable-next-line no-alert
           if (data.payload.ableToDeclareWin) {
-            alert('Соперник давно в оффлайне - ты можешь требовать победу!');
+            // eslint-disable-next-line no-alert
+            // alert('Соперник давно в оффлайне - ты можешь требовать победу!');
           } else {
-            alert('про... ты свое счастье.......');
+            // eslint-disable-next-line no-alert
+            // alert('про... ты свое счастье.......');
           }
           break;
         default:
@@ -313,7 +315,20 @@ const PlayRoomPage = () => {
           </div>
         </div>
         <div className="board">
+          <div className="board__row" data-row={9}>
+            <div className="empty-cell" data-cell="top-empty" />
+            <div className="xA-cell" data-cell="top-a">a</div>
+            <div className="xA-cell" data-cell="top-a">b</div>
+            <div className="xA-cell" data-cell="top-a">c</div>
+            <div className="xA-cell" data-cell="top-a">d</div>
+            <div className="xA-cell" data-cell="top-a">e</div>
+            <div className="xA-cell" data-cell="top-a">f</div>
+            <div className="xA-cell" data-cell="top-a">g</div>
+            <div className="xA-cell" data-cell="top-a">h</div>
+            <div className="empty-cell" data-cell="top-empty" />
+          </div>
           <div className="board__row" data-row={8}>
+            <div className="x1-cell" data-cell="left-8">8</div>
             <div className="cell" data-cell="a8" />
             <div className="cell" data-cell="b8" />
             <div className="cell" data-cell="c8" />
@@ -322,9 +337,10 @@ const PlayRoomPage = () => {
             <div className="cell" data-cell="f8" />
             <div className="cell" data-cell="g8" />
             <div className="cell" data-cell="h8" />
-            {/* <div className="cell" data-cell="h8" /> */}
+            <div className="x1-cell" data-cell="right-8">8</div>
           </div>
           <div className="board__row" data-row={7}>
+            <div className="x1-cell" data-cell="left-7">7</div>
             <div className="cell" data-cell="a7" />
             <div className="cell" data-cell="b7" />
             <div className="cell" data-cell="c7" />
@@ -333,8 +349,10 @@ const PlayRoomPage = () => {
             <div className="cell" data-cell="f7" />
             <div className="cell" data-cell="g7" />
             <div className="cell" data-cell="h7" />
+            <div className="x1-cell" data-cell="right-7">7</div>
           </div>
           <div className="board__row" data-row={6}>
+            <div className="x1-cell" data-cell="left-6">6</div>
             <div className="cell" data-cell="a6" />
             <div className="cell" data-cell="b6" />
             <div className="cell" data-cell="c6" />
@@ -343,8 +361,10 @@ const PlayRoomPage = () => {
             <div className="cell" data-cell="f6" />
             <div className="cell" data-cell="g6" />
             <div className="cell" data-cell="h6" />
+            <div className="x1-cell" data-cell="right-6">6</div>
           </div>
           <div className="board__row" data-row={5}>
+            <div className="x1-cell" data-cell="left-5">5</div>
             <div className="cell" data-cell="a5" />
             <div className="cell" data-cell="b5" />
             <div className="cell" data-cell="c5" />
@@ -353,8 +373,10 @@ const PlayRoomPage = () => {
             <div className="cell" data-cell="f5" />
             <div className="cell" data-cell="g5" />
             <div className="cell" data-cell="h5" />
+            <div className="x1-cell" data-cell="right-5">5</div>
           </div>
           <div className="board__row" data-row={4}>
+            <div className="x1-cell" data-cell="left-4">4</div>
             <div className="cell" data-cell="a4" />
             <div className="cell" data-cell="b4" />
             <div className="cell" data-cell="c4" />
@@ -363,8 +385,10 @@ const PlayRoomPage = () => {
             <div className="cell" data-cell="f4" />
             <div className="cell" data-cell="g4" />
             <div className="cell" data-cell="h4" />
+            <div className="x1-cell" data-cell="right-4">4</div>
           </div>
           <div className="board__row" data-row={3}>
+            <div className="x1-cell" data-cell="left-3">3</div>
             <div className="cell" data-cell="a3" />
             <div className="cell" data-cell="b3" />
             <div className="cell" data-cell="c3" />
@@ -373,8 +397,10 @@ const PlayRoomPage = () => {
             <div className="cell" data-cell="f3" />
             <div className="cell" data-cell="g3" />
             <div className="cell" data-cell="h3" />
+            <div className="x1-cell" data-cell="right-3">3</div>
           </div>
           <div className="board__row" data-row={2}>
+            <div className="x1-cell" data-cell="left-2">2</div>
             <div className="cell" data-cell="a2" />
             <div className="cell" data-cell="b2" />
             <div className="cell" data-cell="c2" />
@@ -383,8 +409,10 @@ const PlayRoomPage = () => {
             <div className="cell" data-cell="f2" />
             <div className="cell" data-cell="g2" />
             <div className="cell" data-cell="h2" />
+            <div className="x1-cell" data-cell="right-2">2</div>
           </div>
           <div className="board__row" data-row={1}>
+            <div className="x1-cell" data-cell="left-1">1</div>
             <div className="cell" data-cell="a1" />
             <div className="cell" data-cell="b1" />
             <div className="cell" data-cell="c1" />
@@ -393,6 +421,19 @@ const PlayRoomPage = () => {
             <div className="cell" data-cell="f1" />
             <div className="cell" data-cell="g1" />
             <div className="cell" data-cell="h1" />
+            <div className="x1-cell" data-cell="right-1">1</div>
+          </div>
+          <div className="board__row" data-row={0}>
+            <div className="empty-cell" data-cell="top-empty" />
+            <div className="xA-cell" data-cell="top-a">a</div>
+            <div className="xA-cell" data-cell="top-a">b</div>
+            <div className="xA-cell" data-cell="top-a">c</div>
+            <div className="xA-cell" data-cell="top-a">d</div>
+            <div className="xA-cell" data-cell="top-a">e</div>
+            <div className="xA-cell" data-cell="top-a">f</div>
+            <div className="xA-cell" data-cell="top-a">g</div>
+            <div className="xA-cell" data-cell="top-a">h</div>
+            <div className="empty-cell" data-cell="top-empty" />
           </div>
           <div className="fen">
             <form className="fen__form">
