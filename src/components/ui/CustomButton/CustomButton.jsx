c101334bb1e7162aaa9e356bd1060ecdff35e49a
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2024-06-19 01:28:38 +0300

    add tooltip to CustomButton

--- src/components/ui/CustomButton/CustomButton.jsx
+++ src/components/ui/CustomButton/CustomButton.jsx
@@ -1,9 +1,23 @@
 import { Button } from 'antd';
+import { Tooltip } from 'react-tooltip';
 
 const CustomButton = ({
-  text, className, onClick, hide = false,
+  text, className, onClick, hide = false, disabled = false, tooltip,
 }) => (
-  <Button style={{ width: '100%', display: hide ? 'none' : 'flex' }} className={className} onClick={() => onClick()}>{text}</Button>
+  <>
+    <Button
+      style={{ width: '100%', display: hide ? 'none' : 'flex' }}
+      className={className}
+      onClick={() => onClick()}
+      disabled={disabled}
+      data-tooltip-id="buttonTooltip"
+      data-tooltip-content={tooltip}
+    >
+      {text}
+    </Button>
+
+    <Tooltip id="buttonTooltip" />
+  </>
 );
 
 export default CustomButton;
